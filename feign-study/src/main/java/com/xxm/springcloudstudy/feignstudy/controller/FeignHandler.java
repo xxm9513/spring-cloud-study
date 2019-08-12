bac1735e799284523e8148c122c84f613f1e9827
xuxiaoming <xxmpersonmail@foxmail.com>
2019-08-12 18:44:33 +0800

    远程配置中心服务

--- feign-study/src/main/java/com/xxm/springcloudstudy/feignstudy/controller/FeignHandler.java
+++ feign-study/src/main/java/com/xxm/springcloudstudy/feignstudy/controller/FeignHandler.java
@@ -3,6 +3,7 @@ package com.xxm.springcloudstudy.feignstudy.controller;
 import com.xxm.springcloudstudy.entity.Student;
 import com.xxm.springcloudstudy.feignstudy.feign.FeignProviderClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,9 @@ public class FeignHandler {
     @Autowired
     private FeignProviderClient feignProviderClient;
 
+    @Value("${foo}")
+    private String foo;
+
     @GetMapping("findAll")
     public Collection<Student> findAll() {
         return feignProviderClient.findAll();
@@ -27,5 +31,10 @@ public class FeignHandler {
     @GetMapping("/index")
     public String index(){
         return feignProviderClient.index();
+    }
+
+    @GetMapping("/getFoo")
+    public String getFoo(){
+        return foo;
     }
 }
