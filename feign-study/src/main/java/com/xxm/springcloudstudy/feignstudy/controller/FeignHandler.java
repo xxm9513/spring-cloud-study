d64b14978c1163df5ff386c2d140cfd1f73915d1
xuxiaoming <xxmpersonmail@foxmail.com>
2019-08-12 19:26:57 +0800

    添加监控模块实时刷新配置, 测试

--- feign-study/src/main/java/com/xxm/springcloudstudy/feignstudy/controller/FeignHandler.java
+++ feign-study/src/main/java/com/xxm/springcloudstudy/feignstudy/controller/FeignHandler.java
@@ -4,6 +4,7 @@ import com.xxm.springcloudstudy.entity.Student;
 import com.xxm.springcloudstudy.feignstudy.feign.FeignProviderClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.Collection;
  */
 @RestController
 @RequestMapping("feign")
+@RefreshScope
 public class FeignHandler {
     @Autowired
     private FeignProviderClient feignProviderClient;
