0227672b3b9da413390132345427f8f6dc312ea5
xuxiaoming <xxmpersonmail@foxmail.com>
2019-08-13 12:23:21 +0800

    zipkin  client

---
+++ zipkin-client/src/main/java/com/xxm/springcloudstudy/zipkinclient/ZipkinClientApplication.java
@@ -0,0 +1,13 @@
+package com.xxm.springcloudstudy.zipkinclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinClientApplication.class, args);
+    }
+
+}
