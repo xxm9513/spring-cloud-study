d4d3b9847993c84db0a18584b4df021650346d5b
xuxiaoming <xxmpersonmail@foxmail.com>
2019-08-13 11:53:38 +0800

    更改配置

---
+++ feign-study/src/main/java/com/xxm/springcloudstudy/feignstudy/FeignApplication2.java
@@ -0,0 +1,13 @@
+package com.xxm.springcloudstudy.feignstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class FeignApplication2 {
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication2.class, args);
+    }
+}
